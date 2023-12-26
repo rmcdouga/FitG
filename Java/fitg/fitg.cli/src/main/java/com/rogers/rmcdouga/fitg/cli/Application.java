3a779a60b7a3ba548ea853eb99b2dab4dbe6a313
Rob McDougall <rmcdougawww@rogers.com>
2023-12-26 10:35:38 -0500

    Added first command skeleton (create-game)

--- Java/fitg/fitg.cli/src/main/java/com/rogers/rmcdouga/fitg/cli/Application.java
+++ Java/fitg/fitg.cli/src/main/java/com/rogers/rmcdouga/fitg/cli/Application.java
@@ -2,8 +2,10 @@ package com.rogers.rmcdouga.fitg.cli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@EnableCommand(GameCommands.class)
 public class Application {
 
   public static void main(String[] args) {
