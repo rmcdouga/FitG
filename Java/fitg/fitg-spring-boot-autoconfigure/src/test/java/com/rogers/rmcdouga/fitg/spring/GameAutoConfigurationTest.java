7ded48b4c5054bd571cd11f57ff5e368e0fb1d61
Rob McDougall <rmcdougawww@rogers.com>
2023-12-26 09:59:29 -0500

    Filled out the project a little.

---
+++ Java/fitg/fitg-spring-boot-autoconfigure/src/test/java/com/rogers/rmcdouga/fitg/spring/GameAutoConfigurationTest.java
@@ -0,0 +1,27 @@
+package com.rogers.rmcdouga.fitg.spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rogers.rmcdouga.fitg.basegame.Game;
+
+@SpringBootTest(classes = {com.rogers.rmcdouga.fitg.spring.GameAutoConfigurationTest.TestApplication.class, GameAutoConfiguration.class})
+class GameAutoConfigurationTest {
+
+	@Test
+	void testGame(@Autowired Game game) {
+		assertNotNull(game);
+	}
+
+	@SpringBootApplication
+	public static class TestApplication {
+		public static void main(String[] args) {
+			SpringApplication.run(TestApplication.class, args);
+		}
+	}
+}
