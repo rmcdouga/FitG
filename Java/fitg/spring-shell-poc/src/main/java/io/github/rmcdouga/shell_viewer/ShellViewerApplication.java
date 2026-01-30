755ab31661951e3cc01d8708a869aba3541c9b73
Rob McDougall <rmcdougawww@rogers.com>
2026-01-30 09:35:37 -0500

    🎉 Create initial skeleton of project

---
+++ Java/fitg/spring-shell-poc/src/main/java/io/github/rmcdouga/shell_viewer/ShellViewerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rmcdouga.shell_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellViewerApplication.class, args);
+	}
+
+}
---
+++ Java/fitg/spring-shell-poc/src/test/java/io/github/rmcdouga/shell_viewer/ShellViewerApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.rmcdouga.shell_viewer;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class ShellViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
