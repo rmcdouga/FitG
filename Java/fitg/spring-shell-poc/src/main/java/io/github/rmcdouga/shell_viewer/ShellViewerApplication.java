7259997aeeefc1b577aa66af170fc00ee3aa4628
Rob McDougall <rmcdougawww@rogers.com>
2026-01-31 08:00:01 -0500

    ✨ Prints Hello World

--- Java/fitg/spring-shell-poc/src/main/java/io/github/rmcdouga/shell_viewer/ShellViewerApplication.java
+++ Java/fitg/spring-shell-poc/src/main/java/io/github/rmcdouga/shell_viewer/ShellViewerApplication.java
@@ -2,6 +2,7 @@ package io.github.rmcdouga.shell_viewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.core.command.annotation.Command;
 
 @SpringBootApplication
 public class ShellViewerApplication {
@@ -9,5 +10,9 @@ public class ShellViewerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShellViewerApplication.class, args);
 	}
-
+	
+	@Command
+	public void displayGame() {
+		System.out.println("Hello world!");
+	}
 }
