b9288206f5321e8c25d1ae76be1e4628cda8f837
Rob McDougall <rmcdougawww@rogers.com>
2026-06-14 15:34:39 -0400

    🎨 Trivial whitespace formatting

--- Java/fitg/fitg-ai-client/src/main/java/io/github/rmcdouga/fitg/aiclient/FitGClientFxApplication.java
+++ Java/fitg/fitg-ai-client/src/main/java/io/github/rmcdouga/fitg/aiclient/FitGClientFxApplication.java
@@ -13,27 +13,27 @@ public class FitGClientFxApplication extends Application {
 	private ConfigurableApplicationContext applicationContext;
 	private Stage mainStage;
 
-    @Override
-    public void init() {
-        applicationContext = new SpringApplicationBuilder(FitGClientSpringApplication.class).run();
-    }
+	@Override
+	public void init() {
+		applicationContext = new SpringApplicationBuilder(FitGClientSpringApplication.class).run();
+	}
 
-    @Override
-    public void stop() {
-        applicationContext.close();
-        mainStage.close();
-    }
+	@Override
+	public void stop() {
+		applicationContext.close();
+		mainStage.close();
+	}
 
-    @Override
-    public void start(Stage stage) throws Exception {
-    	this.mainStage = stage;
-    	
-    	FxmlLoader fxmlLoader = applicationContext.getBean(FxmlLoader.class);
-    	Parent parent = fxmlLoader.load("/fxml/mainwindow.fxml");
-        Scene scene = new Scene(parent);
-        stage.setTitle("FitG AI Client");
-        stage.setScene(scene);
-        stage.getIcons().add(new Image("/icon/icon.png"));
-       stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws Exception {
+		this.mainStage = stage;
+
+		FxmlLoader fxmlLoader = applicationContext.getBean(FxmlLoader.class);
+		Parent parent = fxmlLoader.load("/fxml/mainwindow.fxml");
+		Scene scene = new Scene(parent);
+		stage.setTitle("FitG AI Client");
+		stage.setScene(scene);
+		stage.getIcons().add(new Image("/icon/icon.png"));
+		stage.show();
+	}
 }
