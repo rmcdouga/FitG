e8316b9763a7103d2426d9307e58b52dbc43cb36
Rob McDougall <rmcdougawww@rogers.com>
2026-06-14 16:49:46 -0400

    ✅ Updated wiring test to use Spring context
    Instead of loading the FXML manually, this approach relies on Spring
    Boot to do the wiring so it is more of an integration test.

--- Java/fitg/fitg-ai-client/src/test/java/io/github/rmcdouga/fitg/aiclient/gui/MainWindowFxmlWiringTest.java
+++ Java/fitg/fitg-ai-client/src/test/java/io/github/rmcdouga/fitg/aiclient/gui/MainWindowFxmlWiringTest.java
@@ -1,73 +1,53 @@
 package io.github.rmcdouga.fitg.aiclient.gui;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Consumer;
 
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxRobot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
+import io.github.rmcdouga.fitg.aiclient.FxmlLoader;
 import io.github.rmcdouga.fitg.aiclient.gui.ports.ChatClient;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextArea;
-import javafx.scene.image.ImageView;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
+@SpringBootTest
 @ExtendWith(ApplicationExtension.class)
 @ExtendWith(SoftAssertionsExtension.class)
 class MainWindowFxmlWiringTest {
 
-    @Test
-    void mainWindowFxmlLoadsAndWiresController(FxRobot robot, SoftAssertions softly) {
-        var rootRef = new AtomicReference<Parent>();
-        var controllerRef = new AtomicReference<Object>();
+	@MockitoBean
+	ChatClient mockChatClient;
+	@Autowired
+	FxmlLoader fxmlLoader;
 
-        robot.interact(() -> {
-            var loader = new FXMLLoader(getClass().getResource("/fxml/mainwindow.fxml"));
-            loader.setControllerFactory(type -> {
-                if (type == MainApplicationController.class) {
-                    return new MainApplicationController(new StubChatClient());
-                }
-                try {
-                    return type.getDeclaredConstructor().newInstance();
-                } catch (ReflectiveOperationException e) {
-                    throw new IllegalStateException("Unable to create controller: " + type.getName(), e);
-                }
-            });
-            try {
-                rootRef.set(loader.load());
-                controllerRef.set(loader.getController());
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        });
+	@Start
+	public void start(Stage stage) throws IOException {
+		Parent parent = fxmlLoader.load("/fxml/mainwindow.fxml");
+		Scene scene = new Scene(parent);
+		stage.setTitle("FitG AI Client");
+		stage.setScene(scene);
+		stage.getIcons().add(new Image("/icon/icon.png"));
+		stage.show();
+	}
 
-        softly.assertThat(controllerRef.get()).isInstanceOf(MainApplicationController.class);
-        softly.assertThat(rootRef.get().lookup("#textAreaAiResponse")).isInstanceOf(TextArea.class);
-        softly.assertThat(rootRef.get().lookup("#textAreaInput")).isInstanceOf(TextArea.class);
-        softly.assertThat(rootRef.get().lookup("#progressBar")).isInstanceOf(ProgressBar.class);
-        softly.assertThat(rootRef.get().lookup("#imageView")).isInstanceOf(ImageView.class);
-    }
-
-    private static class StubChatClient implements ChatClient {
-        @Override
-        public void sendPrompt(String textPrompt, byte[] media, Runnable onComplete,
-                Consumer<? super Throwable> onError, Consumer<? super String> consumer) {
-            consumer.accept("");
-            onComplete.run();
-        }
-
-        @Override
-        public void sendPrompt(String textPrompt, Runnable onComplete, Consumer<? super Throwable> onError,
-                Consumer<? super String> consumer) {
-            consumer.accept("");
-            onComplete.run();
-        }
-    }
+	@Test
+	void mainWindowFxmlLoadsAndWiresController(SoftAssertions softly,
+											   @Autowired MainApplicationController mainApplicationController
+											  ) throws IOException {
+		// Assert
+		softly.assertThat(mainApplicationController).isNotNull();
+		softly.assertThat(mainApplicationController.textAreaInput).isNotNull();
+		softly.assertThat(mainApplicationController.textAreaAiResponse).isNotNull();
+		softly.assertThat(mainApplicationController.progressBar).isNotNull();
+		softly.assertThat(mainApplicationController.imageView).isNotNull();
+	}
 }
