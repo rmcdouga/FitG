06da904f26624743830f40356989be2c70f00eeb
Rob McDougall <rmcdougawww@rogers.com>
2026-07-03 08:54:27 -0400

    ✅ Cleaned up System Prompt test
    Removed unnecessary evaluation of response.

--- Java/fitg/fitg-ai-client/src/test/java/io/github/rmcdouga/fitg/aiclient/SystemPromptIntegrationTest.java
+++ Java/fitg/fitg-ai-client/src/test/java/io/github/rmcdouga/fitg/aiclient/SystemPromptIntegrationTest.java
@@ -1,29 +1,16 @@
 package io.github.rmcdouga.fitg.aiclient;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static io.github.rmcdouga.fitg.aiclient.EvaluationResponseAssert.assertThat;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.evaluation.FactCheckingEvaluator;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.evaluation.EvaluationResponse;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
@@ -67,13 +54,12 @@ class SystemPromptIntegrationTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"liquid10", "Liquid 1 0 ", "Liquid_1_0", "Liquid 1-0"})
 	void testSystemPrompt_ValidUnits(String unit, FxRobot robot,
-			  @Autowired MainApplicationController mainApplicationController,
-			  @Autowired FactCheckingEvaluator factCheckingEvaluator
+			  @Autowired MainApplicationController mainApplicationController
 			  ) throws TimeoutException {
 		var testQuery = "Is %s is a valid unit? Respond with Yes or No.".formatted(unit);
 		// Arrange
 		var aiResponse = sendQuery(robot, mainApplicationController, testQuery);
-		IO.println("AI Response: " + aiResponse);
+//		IO.println("AI Response: " + aiResponse);
 
 		assertThat(aiResponse).containsIgnoringCase("yes");
 	}
@@ -81,39 +67,12 @@ class SystemPromptIntegrationTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"liquid00", "Liquid 0 0 ", "Liquid_2_0", "Liquid 2-0"})
 	void testSystemPrompt_InValidUnits(String unit, FxRobot robot,
-			  @Autowired MainApplicationController mainApplicationController,
-			  @Autowired FactCheckingEvaluator factCheckingEvaluator
+			  @Autowired MainApplicationController mainApplicationController
 			  ) throws TimeoutException {
 		var testQuery = "Is %s is a valid unit? Respond with Yes or No.".formatted(unit);
 		var aiResponse = sendQuery(robot, mainApplicationController, testQuery);
-		IO.println("AI Response: " + aiResponse);
+//		IO.println("AI Response: " + aiResponse);
 
 		assertThat(aiResponse).containsIgnoringCase("no");
-	}
-	
-	@TestConfiguration
-	static class LocalTestConfig {
-		@Bean
-		static FactCheckingEvaluator factCheckingEvaluator() {
-			  // Set up the Ollama API
-			  OllamaApi ollamaApi = OllamaApi.builder().baseUrl("http://localhost:11434").build();
-
-			  OllamaChatOptions chatOptions = OllamaChatOptions.builder()
-							 								   .model("bespoke-minicheck") // Use bespoke-minicheck model for fact-checking
-							 								   .numPredict(2)
-							 								   .temperature(0.0d)
-							 								   .build();
-			  
-			  ChatModel chatModel = OllamaChatModel.builder()
-					  							   .ollamaApi(ollamaApi)
-					  							   .options(chatOptions)
-					  							   .build();
-
-
-			  // Create the FactCheckingEvaluator
-			  var builder = ChatClient.builder(chatModel);
-			  
-			  return FactCheckingEvaluator.builder(builder).build();
-		}
 	}
 }
